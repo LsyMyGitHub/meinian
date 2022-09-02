bbac26ecde8b8e57f2b9169df77858a8559d7e47
林嵩嵛 <647851381@qq.com>
2022-09-02 22:08:51 +0800

    美年旅游项目初始化

---
+++ meinian_common/src/test/java/com/atguigu/qiniu/ABC.java
@@ -0,0 +1,14 @@
+package com.atguigu.qiniu;
+
+import org.junit.Test;
+
+/**
+ * @author lin
+ * @create 2022-09-02 22:08
+ */
+public class ABC {
+    @Test
+    public void test(){
+        System.out.println("ABC");
+    }
+}
